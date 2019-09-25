cad4550e9106c2c3e411e4072c4b2fe6021c8ebd
sitRyo <al15030@shibaura-it.ac.jp>
2019-09-25 19:15:38 +0900

    revise header

--- portfolio-front/src/App.vue
+++ portfolio-front/src/App.vue
@@ -1,31 +1,85 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+      <h1>Ryo Portfolio</h1>
+      <h2>Enginnering Student</h2>
+    </header>
+    <nav>
+      <router-link to="/" class="nav-element">ホーム</router-link>
+      <router-link to="/about" class="nav-element">製作物</router-link>
+      <router-link to="/blog" class="nav-element">ブログ</router-link>
+      <router-link to="/contact" class="nav-element">連絡先</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
+  position: relative;
+  height: 30vh;
+  background: url(./assets/header-portfolio2.jpg) center / cover;
+}
+
+header h1 {
+  position: absolute;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+	z-index: 1;
+	top: 50%;
+	width: 100%;
+	margin: 0;
+	padding: 0.5em;
+	color: #fff;
+	font-size: 5rem;
+	font-family: "Dosis", sans-serif;
+	letter-spacing: .1em;
+	transform: translateY(-50%);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+header h2 {
+  position: absolute;
+  text-align: center;
+  z-index: 1;
+  top: 70%;
+  width: 100%;
+  margin: 0;
+  padding: 0.5em;
+  color: #fff;
+  font-size: 2rem;
+  transform: translateY(-50%);
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+nav {
+  width: 100%;
+  background-color: #fff;
+  font-family: 'Quicksand', "游ゴシック体", "Yu Gothic", YuGothic,
+   "ヒラギノ角ゴシック Pro", "Hiragino Kaku Gothic Pro", 'メイリオ' , 
+   Meiryo , Osaka, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+  z-index: 10;
+  box-shadow: 0px 2px 2px #9999;  
+  text-align: center;
+  margin-block-start: .2em;
+  margin-block-end: .2em;
+  padding-block-start: .5em;
+  padding-block-end: .5em;
 }
+
+.nav-element {
+  display: inline-block;
+  text-decoration: none;
+  font-weight: 600;
+  margin-inline-start: 2em;
+  margin-inline-end: 2em;
+  padding-inline-start: 1em;
+  padding-inline-end: 1em;
+  padding-block-start: .5em;
+  padding-block-end: .5em;
+  transition-duration: 1s;
+  border-radius: 3px;
+}
+
+a:hover {
+  background-color:#cccccc;
+}
+
 </style>
--- portfolio-front/src/router.ts
+++ portfolio-front/src/router.ts
@@ -14,12 +14,19 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/skill',
+      name: 'skill',
+      component: () => import('./views/Skill.vue'),
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: () => import('./views/Blog.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('./views/Contact.vue'),
     },
   ],
 });
